baa9b0c4c1a16417baf19950dddc9acf771ce117
yasarih <yasaranglebelearn@gmail.com>
2024-11-09 12:40:35 +0530

    Update data.py

--- data.py
+++ data.py
@@ -58,7 +58,7 @@ def get_merged_data_with_em():
     main_data = fetch_data_from_sheet("17_Slyn6u0G6oHSzzXIpuuxPhzxx4ayOKYkXfQTLtk-Y", "Student class details")
     em_data = fetch_data_from_sheet("17_Slyn6u0G6oHSzzXIpuuxPhzxx4ayOKYkXfQTLtk-Y", "Student Data")
     
-    main_data = main_data.rename(columns={'Student id': 'Student ID'})
+    main_data = main_data.rename(columns={'Student id': 'Student id'})
     em_data = em_data.rename(columns={'Student id': 'Student ID', 'EM': 'EM', 'EM Phone': 'Phone Number'})
 
     merged_data = main_data.merge(em_data[['Student ID', 'EM', 'Phone Number']], on="Student ID", how="left")
