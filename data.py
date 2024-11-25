c721c0d1cea6689af089dcd2bf5de0ce92b86ca4
yasarih <yasaranglebelearn@gmail.com>
2024-11-25 17:57:17 +0530

    Update data.py

--- data.py
+++ data.py
@@ -256,7 +256,7 @@ def show_teacher_schedule(teacher_id):
 # Function to manage data based on the selected role
 def manage_data(data, role):
     st.subheader(f"{role} Data")
-    #st.write("Available columns in data:", data.columns.tolist())  # Debugging
+    st.write("Available columns in data:", data.columns.tolist())  # Debugging
 
     if "MM" in data.columns:
         month = st.sidebar.selectbox("Select Month", sorted(data["MM"].unique()))
